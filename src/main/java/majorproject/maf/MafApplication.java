2b6066609f53dea485617bf705f113d1a0e77dbd
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-10-24 22:58:26 +0530

    initial commit

---
+++ src/main/java/majorproject/maf/MafApplication.java
@@ -0,0 +1,13 @@
+package majorproject.maf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/majorproject/maf/MafApplicationTests.java
@@ -0,0 +1,13 @@
+package majorproject.maf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
