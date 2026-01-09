d0ea5b3f29ea37954ec09121e258551dfaaecaca
V-Satwik-Reddy <satwikstudies05@gmail.com>
2026-01-09 19:38:58 +0530

    added redis and configures it to last for 7 days added its docker file and maven dependencies

--- src/main/java/majorproject/maf/MafApplication.java
+++ src/main/java/majorproject/maf/MafApplication.java
@@ -2,10 +2,10 @@ package majorproject.maf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableCaching
 public class MafApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/majorproject/maf/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package majorproject.maf.config;
+
+import majorproject.maf.dto.response.UserDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofDays(7))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        new Jackson2JsonRedisSerializer<>(UserDto.class)));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
