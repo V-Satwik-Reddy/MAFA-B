f8c119f89a15c84e2e4cd456b9ec290450c52e33
V-Satwik-Reddy <satwikstudies05@gmail.com>
2026-01-10 12:04:14 +0530

    rewrote dashboards fuctionality and code where it was calling 8 db queries and more comple now it is modular and  uses batch proessing and also does only 3[1;5C calls optimised the time from around 7-8ms to 3.5-4.5ms almost 50% imporvement also using caches user data to get user details
    side not i am thinking about rather than cache just keep user id in the auth details in security context holder beacuse of its usecase

--- src/main/java/majorproject/maf/service/DashboardService.java
+++ src/main/java/majorproject/maf/service/DashboardService.java
@@ -2,9 +2,9 @@ package majorproject.maf.service;
 
 import majorproject.maf.dto.response.StockDto;
 import majorproject.maf.dto.response.TransactionDto;
+import majorproject.maf.dto.response.UserDto;
 import majorproject.maf.model.Stock;
 import majorproject.maf.model.Transaction;
-import majorproject.maf.model.User;
 import org.springframework.stereotype.Service;
 import majorproject.maf.repository.*;
 
@@ -15,22 +15,19 @@ import java.util.List;
 public class DashboardService {
 
     private final TransactionRepository transactionRepo;
-    private final UserRepository userRepo;
     private final StockRepository stockRepo;
-    private final PriceFetch priceFetch;
+    private final StockPriceRepository stockPriceRepository;
+    private final UserCacheService userCacheService;
 
-    public DashboardService(TransactionRepository transactionRepo, UserRepository userRepo, StockRepository stockRepo, PriceFetch priceFetch) {
+    public DashboardService(TransactionRepository transactionRepo, StockRepository stockRepo, StockPriceRepository stockPriceRepository,UserCacheService userCacheService) {
         this.transactionRepo = transactionRepo;
-        this.userRepo = userRepo;
         this.stockRepo = stockRepo;
-        this.priceFetch = priceFetch;
+        this.stockPriceRepository = stockPriceRepository;
+        this.userCacheService = userCacheService;
     }
 
     public List<TransactionDto> getUserTransactions(String email) {
-        User user = userRepo.findByEmail(email);
-        if (user == null) {
-            throw new RuntimeException("User not found");
-        }
+        UserDto user = userCacheService.getCachedUser(email);
         return transactionRepo.findByUserIdOrderByCreatedAtDesc(user.getId())
                 .stream()
                 .map(txn -> new TransactionDto(
@@ -50,36 +47,28 @@ public class DashboardService {
 
     public List<StockDto> getHoldingsDetails(String email) {
         List<StockDto> ans=new ArrayList<>();
-        int userid= userRepo.findByEmail(email).getId();
-        List<Stock> holdings= stockRepo.findByUserId(userid);
-        List<TransactionDto> alltxns=getUserTransactions(email);
+        UserDto user = userCacheService.getCachedUser(email);
+        int userId = user.getId();
+        List<Stock> holdings= stockRepo.findByUserId(userId);
+        List<TransactionDto> alltxns=transactionRepo.findByUserIdOrderByCreatedAtDesc(user.getId()).stream().map(txn->new TransactionDto(
+                txn.getId(),
+                txn.getType(),
+                txn.getAsset(),
+                txn.getAssetQuantity(),
+                txn.getAmount(),
+                txn.getCreatedAt()
+        )).toList();
+        List<Double> prices= stockPriceRepository.batchFind(holdings.stream().map(Stock::getSymbol).toList());
+
+        int i=0;
         for(Stock stock:holdings){
             StockDto stockDto=new StockDto();
             stockDto.setSymbol(stock.getSymbol());
             stockDto.setShares(stock.getShares());
-            double totalAmount=0.0;
-            long buyQty = stock.getShares();
-            for(TransactionDto txn:alltxns){
-                if(txn.getAsset().equals(stock.getSymbol()) && txn.getType().equalsIgnoreCase("BUY")){
-                    long q=txn.getAssetQuantity();
-                    if(buyQty>=q){
-                        totalAmount+=txn.getAmount();
-                        buyQty-=q;
-                    }else {
-                        totalAmount += (txn.getAmount() / q) * buyQty;
-                        buyQty = 0;
-                    }
-                }
-                if(buyQty<=0) break;
-            }
-            double avgBuyPrice=totalAmount/stock.getShares();
+            double avgBuyPrice = getAvgBuyPrice(stock, alltxns);
             stockDto.setAvgBuyPrice(avgBuyPrice);
             double currentPrice;
-            try {
-                currentPrice = priceFetch.fetchCurrentPrice(stock.getSymbol());
-            } catch (Exception e) {
-                currentPrice = 0.0; // or last known price
-            }
+            currentPrice = prices.get(i++);
             stockDto.setTotalAmount(stock.getShares()*currentPrice);
             stockDto.setCurrentPrice(currentPrice);
             double gainLoss=(currentPrice - avgBuyPrice)*stock.getShares();
@@ -87,5 +76,24 @@ public class DashboardService {
             ans.add(stockDto);
         }
         return ans;
+    }
+
+    private static double getAvgBuyPrice(Stock stock, List<TransactionDto> alltxns) {
+        double totalAmount=0.0;
+        long buyQty = stock.getShares();
+        for(TransactionDto txn: alltxns){
+            if(txn.getAsset().equals(stock.getSymbol()) && txn.getType().equalsIgnoreCase("BUY")){
+                long q=txn.getAssetQuantity();
+                if(buyQty>=q){
+                    totalAmount+=txn.getAmount();
+                    buyQty-=q;
+                }else {
+                    totalAmount += (txn.getAmount() / q) * buyQty;
+                    buyQty = 0;
+                }
+            }
+            if(buyQty<=0) break;
+        }
+        return totalAmount/ stock.getShares();
     }
 }
