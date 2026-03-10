4b1718d05ffb0611174729b9b77c3dca0f60149e
V-Satwik-Reddy <satwikstudies05@gmail.com>
2026-03-11 01:04:58 +0530

    changes to execution service and stock repo in the selling shares part

--- src/main/java/majorproject/maf/repository/StockRepository.java
+++ src/main/java/majorproject/maf/repository/StockRepository.java
@@ -4,6 +4,7 @@ import majorproject.maf.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -23,16 +24,23 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
 
     @Modifying
     @Query("""
-        UPDATE Stock h
-        SET h.shares = h.shares - :quantity
-        WHERE h.user.id = :userId AND h.symbol = :symbol AND h.shares >= :quantity
-    """)
-    int decrementIfSufficientShares(int id, String symbol, long quantity);
+    UPDATE Stock h
+    SET h.shares = h.shares - :quantity
+    WHERE h.user.id = :userId AND h.symbol = :symbol AND h.shares >= :quantity
+""")
+    int decrementIfSufficientShares(
+            @Param("userId") int userId,
+            @Param("symbol") String symbol,
+            @Param("quantity") long quantity
+    );
 
     @Modifying
     @Query("""
-    Delete from Stock s
-    where s.user.id=:userId and s.symbol=:symbol and s.shares=0
+    DELETE FROM Stock h
+    WHERE h.user.id = :userId AND h.symbol = :symbol AND h.shares <= 0
 """)
-    void deleteIfSharesZero(int id, String symbol);
+    void deleteIfSharesZero(
+            @Param("userId") int userId,
+            @Param("symbol") String symbol
+    );
 }
